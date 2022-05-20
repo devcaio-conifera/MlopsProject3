59f91c0bfcb34a41adc7bcddeff2d1eda03e7d51
caio <caio.carduz@gmail.com>
2022-05-20 13:12:57 -0300

    main Dyno updated

--- main.py
+++ main.py
@@ -12,11 +12,13 @@ from fastapi.encoders import jsonable_encoder
 
 
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("pip install 'dvc[s3]'")
     os.system("dvc config core.no_scm true")
     if os.system("dvc pull") != 0:
         exit("dvc pull failed")
     os.system("rm -r .dvc .apt/usr/lib/dvc")
-    
+
+os.system("rm -r .dvc .apt/usr/lib/dvc")  
 # path = os.getcwd()
 # print(sys.path)
 def underscore_to_hyphen_replace(string: str) -> str:
